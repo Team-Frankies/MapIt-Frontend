c5dc5359e723e7387f509ce0e17bd902b73b96e6
Nicolás Díaz <nikodiaz56@gmail.com>
2023-04-24 19:18:39 -0200

    feat(login): add regexp to validations

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -8,16 +8,26 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.minLength(8)]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern(
+        /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+    ]),
+    password: new FormControl('', [
+      Validators.minLength(8),
+      Validators.required,
+    ]),
   });
+  hide = true;
 
   getErrorMessage(fieldName: string) {
     const field = this.loginForm.get(fieldName);
     if (field?.touched && field?.hasError('required')) {
       return 'Ingresa un valor';
     }
-    return field?.hasError('email')
+    return field?.hasError('email') || field?.hasError('pattern')
       ? 'Ingresa un email válido'
       : field?.dirty && field?.hasError('minlength')
       ? 'La contraseña debe contener mínimo 8 caracteres'
