5ca9443340e8c2700be2b278d0b8fb2c319391f0
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-05-06 22:54:32 +0300

    Limpiar modulos para corregir un error de doble injección

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -7,8 +7,8 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterComponent } from './components/register/register.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '../../shared/modules/material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     LoginPageComponent,
     RegisterPageComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, AuthRoutingModule, AngularMaterialModule, FormsModule, ReactiveFormsModule],
   exports: [
     RouterModule
   ],
