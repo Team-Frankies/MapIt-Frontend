bb461ddaf95e32707d2883176620e95a935cd8f5
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-05-06 22:54:51 +0300

    Función para hacer logout

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import { logout } from '../../shared/stores/actions/auth.actions';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   apiUrl = environment.apiUrl;
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private store: Store<{ loggedIn: boolean }>
+  ) {}
 
   async register(inputdata: any) {
     const { email, firstname, lastname, password } = inputdata;
@@ -18,5 +23,10 @@ export class AuthService {
       lastname,
       password
     });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.store.dispatch(logout())
   }
 }
