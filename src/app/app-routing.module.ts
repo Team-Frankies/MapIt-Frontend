0de5ec1d9d19f4face949258dcc225b2b9c10a80
Iván Fernández de Pinedo <ivan.fdp@gmail.com>
2023-05-13 22:46:07 +0300

    Generamos el UserModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./features/auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./features/user/user.module').then((m) => m.UserModule),
+  },
 
 ];
 
