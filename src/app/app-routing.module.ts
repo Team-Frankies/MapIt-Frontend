c2fd7f4ef106df1537029d397973de02b71d5f4d
Ruben Mantilla <-h>
2023-05-24 11:32:26 -0500

    agregando home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '' , redirectTo: 'auth', pathMatch: 'full'},
+  { path: '' , redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/home/home.module').then((m) => m.HomeModule),
+  },
   {
     path: 'map',
     loadChildren: () =>
