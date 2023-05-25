c2b45f739f55e8470f3e48092027ccbbe41b1ac6
Maje <mjnct@hotmail.com>
2023-05-25 13:12:43 +0200

    si no obtiene place no abre ventana emergente, salta un aviso al usuario

--- src/app/features/map/map/map.component.ts
+++ src/app/features/map/map/map.component.ts
@@ -3,7 +3,7 @@ import { MarkersService } from '../services/markers.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, map, of, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps';
 import { PlacesService } from '../services/places.service';
 import {GeolocationService} from '@ng-web-apis/geolocation';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -22,7 +22,7 @@ export class MapComponent{
   @Input() address?:  google.maps.LatLngLiteral;
 
   infoContent: string[] =[]
-  place!: PlaceInterface ;
+  place!: PlaceInterface | undefined ;
 
   center={lat: 40.41, lng: -3.7};
   zoom = 15;
@@ -44,9 +44,10 @@ export class MapComponent{
 
   constructor(private httpClient: HttpClient,private mapService: MapService, private markerService: MarkersService, private infoPlace: PlacesService, private readonly geolocation$: GeolocationService, public snackBar: MatSnackBar){
     
-    this.getMap()
     this.getUserLocation()
-
+    this.getMap()
+    
+    
   }
 
   /*******************generando mapa***************************/
@@ -74,21 +75,26 @@ export class MapComponent{
     this.display = event?.latLng?.toJSON() || this.display;
 
   }
+
+  resizeMap(){
+    console.log(1)
+    this.zoom =0;
+    this.zoom = 15;
+  }
 //obtener información de marker
   infoMarker(markerElem: MapMarker, marker: any){
-    //console.log("marker: ")
-    //console.log(marker)
-
+    
     let auxPlace: PlaceInterface;
 
     console.log("id place: " + marker.place_id)
     this.infoPlace.getDataPlace(marker.place_id).subscribe({ 
-      next: (data) => Object.entries(data).map ((elem:any) => {auxPlace = (elem[1] as PlaceInterface), this.setInfoMarker(auxPlace, markerElem)})
-      
-
-      
-      })
-
+      next: (data) => Object.entries(data).map ((elem:any) => {auxPlace = (elem[1] as PlaceInterface), console.log(auxPlace), this.setInfoMarker(auxPlace, markerElem)}), 
+      error: (error) => {
+        this.place=undefined;
+      }
+    
+      });
+    
   }
 
 
@@ -113,16 +119,14 @@ export class MapComponent{
   getUserLocation(){
     this.geolocation$.pipe(take(1)).subscribe(position =>  {
       this.center={lat: position.coords.latitude, lng: position.coords.longitude}
-      , this.display=this.center; this.setMarkers(), this.openMap =true}, (error: GeolocationPositionError) =>{this.setMarkers(),  this.openMap =true, this.snackBar.open('Ubicación no disponible', undefined, {
-        duration:400
-      })});
+      , this.display=this.center; this.setMarkers(), this.openMap =true}, (error: GeolocationPositionError) =>{this.setMarkers(),  this.openMap =true, this.getErrorMessage('Ubicación no disponible')
+       
+    });
   }
 
   //actualiza informacion de punto
   setInfoMarker(place: PlaceInterface, markerElem: MapMarker){
-   // console.log(place)
     this.place = place;
-    console.log(place)
     if(place != null){
       this.placeSetInfo();
       this.info.open(markerElem)
@@ -135,16 +139,21 @@ export class MapComponent{
 
   //actualiza mapa con el buscador de direcciones
   recieveLatLng($event: any) {
+ 
     if($event != undefined){
-
+    this.resizeMap();
     this.center = $event
     this.display = $event
-    this.zoom = 15
+    
     this.setMarkers()}
     else{
-      this.snackBar.open('Ubicación no localizada', undefined, {
-        duration:400})
+      this.getErrorMessage('Ubicación no localizada')
     }
+  }
+
+  getErrorMessage(message: string){
+    this.snackBar.open(message, undefined, {
+      duration:400})
   }
 
   //********************* generando información para map-info-window **********************************/
@@ -152,9 +161,7 @@ export class MapComponent{
 
   placeSetInfo(){
     this.infoContent =[]
-
-    //this.infoContent =  this.place.address_components;
-
+    if(this.place){
     this.place.longName.forEach((element: any) => {
       this.infoContent.push(element.long_name)
     });
@@ -166,17 +173,21 @@ export class MapComponent{
     if(this.infoContent[this.infoContent.length-4] == this.infoContent[this.infoContent.length-5]){
       this.infoContent[this.infoContent.length-5] = ""
     }
-
+  }
       }
 
 
   //********************* ventana emergente **********************************/
   //abrir ventana emergente
   showInfoSite(marker: any){
-    console.log("abrir")
+    if(this.place){
     this.idPlaceInfoWindow = marker.place_id;
     this.placeInfoWindow = this.place;
     this.infoSite = true;
+    }
+    else{
+      this.getErrorMessage('lugar no localizado')
+    }
   }
 
   //cerrar ventana emergente
